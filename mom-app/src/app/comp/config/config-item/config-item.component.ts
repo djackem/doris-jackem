9f901592d3b48cd6b77bb08af8c8ae311c3286df
Chris Jackem <141247065+djackem@users.noreply.github.com>
2023-12-27 18:16:39 -0500

    Config 2
    Config almost done; adding links

--- mom-app/src/app/comp/config/config-item/config-item.component.ts
+++ mom-app/src/app/comp/config/config-item/config-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Item } from 'src/app/types/item';
 
 @Component({
@@ -6,10 +6,52 @@ import { Item } from 'src/app/types/item';
   templateUrl: './config-item.component.html',
   styleUrls: ['./config-item.component.css']
 })
-export class ConfigItemComponent {
+export class ConfigItemComponent implements OnInit{
   @Input() index: number;
   @Input() item: Item;
-  @Output() UpdateJSON = new EventEmitter<any>();
+  @Output() UpdateJSON = new EventEmitter<any>();  
+  active = false;
+  active_char = "+";
+  has_images: boolean;
+  has_links: boolean;
+  link_menu_valid: boolean=false;
+  link_menu_text: string = "";
+  link_menu_url: string = "";
+
+  ngOnInit(){
+    this.has_images = this.item.imgs ? this.item.imgs.length>0 : false;
+    this.has_links = this.item.links ? true : false;
+  }
+
+
+  OpenClick(){
+    this.active = !this.active;
+    this.active_char = this.active ? "-" : "+";
+  }
+
+  OnMainImgChange($event){
+    let v = $event.target.value;
+    let f = v.split(/(\\|\/)/g).pop()
+    this.UpdateJSON.emit({
+      "index": this.index,
+      "prop": "img",
+      "value": f
+    });
+  }
+
+  OnImagesChange($event){
+    let new_list = [];
+    for( let f of $event.target.files){
+      new_list.push(f.name);
+    }
+    this.UpdateJSON.emit({
+      "index": this.index,
+      "prop": "imgs",
+      "value": new_list
+    });
+    this.ngOnInit();
+  }
+
 
   UpdateItemValue(event:any, prop: string ){
     console.log(this.item);
@@ -18,6 +60,18 @@ export class ConfigItemComponent {
       "prop": prop,
       "value": event.target.value
     });
+  }
+
+
+  CheckLinkMenu(){
+    this.link_menu_valid = this.link_menu_text.length>0 && this.link_menu_url.length>0;
+    
+  }
+  CreateLink(){
+    if (this.link_menu_valid){
+      this.link_menu_text = "";
+      this.link_menu_url = "";
+    }
   }
 
 }
--- mom-app/src/app/comp/config/config.component.ts
+++ mom-app/src/app/comp/config/config.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ItemsService } from 'src/app/serv/items.service';
 
 @Component({
@@ -10,21 +10,37 @@ export class ConfigComponent implements OnInit {
   json_final!:any;
   data_copy!:any;
   items:any = [];
+  output_string = "DEFAULT";
+  output_visible: boolean = true;
 
   constructor( public itemService: ItemsService ){}
   
-  ngOnInit(){
+  // On Scroll, hide output
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+    onScroll(event) {
+      this.output_visible = false;
+    }
+
+  ngOnInit(){    
     // Copy items from service
     this.data_copy = JSON.parse(JSON.stringify(this.itemService.Data));
     this.json_final = JSON.stringify(this.data_copy, null, 2);
     for( let item of this.data_copy["items"]){
       this.items.push(item);
-    }
+    }    
+    this.output_string = "Initialized.";
   }
 
-  Update(data){    
-    this.data_copy.items[data.index][data.prop] = data.value;    
-    this.json_final = JSON.stringify(this.data_copy, null, 2);
+  Update(data){
+    try{
+      let old = this.data_copy.items[data.index][data.prop];
+      this.data_copy.items[data.index][data.prop] = data.value;    
+      this.json_final = JSON.stringify(this.data_copy, null, 2);      
+      this.output_visible = true;
+      this.output_string = `Changed: ( ${this.data_copy.items[data.index].name} ) : ${data.prop}\nfrom: ${old}\nto: ${data.value}`;
+    }catch(E){
+      this.output_string = `ERROR:\n${E}`
+    }    
   }
 
 }
